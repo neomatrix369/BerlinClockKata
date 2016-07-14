5c42678966f1066e0b695cf587b563084adb2099
Mani Sarkar <mani@codurance.com>
2016-07-14 23:28:27 +0100

    Further replaced the mechanics for generating the bottom row with a function

--- src/main/java/berlinclock/BerlinClock.java
+++ src/main/java/berlinclock/BerlinClock.java
@@ -7,17 +7,28 @@ import java.time.LocalTime;
 public class BerlinClock {
   public static String displayTimeFor(LocalTime time) {
     if (time.getMinute() == 1) {
-      return getFormattedClock("Y", "O", "O", "O");
+      return getFormattedClockFor(bottomRowLights(1));
     }
 
     if (time.getMinute() == 4) {
-      return getFormattedClock("Y", "Y", "Y", "Y");
+      return getFormattedClockFor(bottomRowLights(4));
     }
 
-    return getFormattedClock("O", "O", "O", "O");
+    return getFormattedClockFor(bottomRowLights(0));
   }
 
-  private static String getFormattedClock(String... state) {
+  private static String[] bottomRowLights(int value) {
+
+    final String[] row = {"O", "O", "O", "O"};
+    for (int index=0; index<row.length; index++) {
+      if (index < value) {
+        row[index] = "Y";
+      }
+    }
+    return row;
+  }
+
+  private static String getFormattedClockFor(String... state) {
     return format("[O]      [O]      [O]      [O]\n[O]      [O]      [O]      [O]\n[O][O][O][O][O][O][O][O][O][O][O]\n[%s]      [%s]      [%s]      [%s]\n", state);
   }
 }
