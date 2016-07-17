1bc7e133c17d14ed7ed17f3d16a076c879c231e6
Mani Sarkar <mani@codurance.com>
2016-07-17 23:55:15 +0100

    Refactored implementation - extracted constants, removed duplications

--- src/main/java/berlinclock/BerlinClock.java
+++ src/main/java/berlinclock/BerlinClock.java
@@ -5,41 +5,54 @@ import static java.lang.String.format;
 import java.time.LocalTime;
 
 public class BerlinClock {
-  public static String displayTimeFor(LocalTime time) {
-    final String topRow         = "[O]      [O]      [O]      [O]";
-    final String firstMiddleRow = "[O]      [O]      [O]      [O]";
+  private static final int FIVE_MINUTES = 5;
 
+  private static final String RED_LIGHT = "R";
+  private static final String YELLOW_LIGHT = "Y";
+  private static final String LIGHT_OFF = "O";
+
+  public static final String TOP_ROW_FORMATTER =
+      format("[%s]      [%s]      [%s]      [%s]", LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF);
+  public static final String FIRST_MIDDLE_ROW_FORMATTER =
+      format("[%s]      [%s]      [%s]      [%s]", LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF);
+  public static final String SECOND_MIDDLE_ROW_FORMATTER =
+      "[%s][%s][%s][%s][%s][%s][%s][%s][%s][%s][%s]";
+
+  public static String displayTimeFor(LocalTime time) {
     int minutes = time.getMinute();
 
     final String secondMiddleRow = secondMiddleRowLights(minutes);
-    final String bottomRow = bottomRowLights(minutes % 5);
+    final String bottomRow = bottomRowLights(minutes % FIVE_MINUTES);
 
     return format("%s\n%s\n%s\n%s\n",
-        topRow,
-        firstMiddleRow,
+        TOP_ROW_FORMATTER,
+        FIRST_MIDDLE_ROW_FORMATTER,
         secondMiddleRow,
         bottomRow);
   }
 
   private static String secondMiddleRowLights(int minutes) {
-    final String[] row = {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"};
-    if (minutes < 5) return getFormattedMiddle(row);
+    final String[] secondMiddleRow = new String[] {LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF,
+        LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF};
+    if (minutes < FIVE_MINUTES) return getFormattedMiddle(secondMiddleRow);
 
-    int count = minutes / 5;
+    switchBlocksInTheMiddleRowOnOrOff(secondMiddleRow, minutes);
 
-    for (int index=0; index<count && index < row.length; index++) {
-      if (replace1369thPositionsWithRed(index)) {
-        row[index] = "R";
-      } else  {
-        row[index] = "Y";
-      }
-    }
-    return getFormattedMiddle(row);
+    return getFormattedMiddle(secondMiddleRow);
   }
 
-  private static String getFormattedMiddle(String[] row) {return format("[%s][%s][%s][%s][%s][%s][%s][%s][%s][%s][%s]", row);}
+  private static void switchBlocksInTheMiddleRowOnOrOff(String[] row, int minutes) {
+    int count = minutes / FIVE_MINUTES;
+    for (int index = 0; index<count && index < row.length; index++) {
+      if (replace1369thPositionsWithRed(index)) {
+        row[index] = RED_LIGHT;
+      } else  {
+        row[index] = YELLOW_LIGHT;
+      }
+    }
+  }
 
-  private static int intoMinutes(int index) {return 5 * (index + 1);}
+  private static String getFormattedMiddle(String[] row) {return format(SECOND_MIDDLE_ROW_FORMATTER, row);}
 
   private static boolean replace1369thPositionsWithRed(int index) {
     return (index == 0) ||
@@ -49,10 +62,10 @@ public class BerlinClock {
   }
 
   private static String bottomRowLights(int minutes) {
-    final String[] row = {"O", "O", "O", "O"};
+    final String[] row = {LIGHT_OFF, LIGHT_OFF, LIGHT_OFF, LIGHT_OFF};
     for (int index=0; index<row.length; index++) {
       if (index < minutes) {
-        row[index] = "Y";
+        row[index] = YELLOW_LIGHT;
       }
     }
     return format("[%s]      [%s]      [%s]      [%s]", row);
