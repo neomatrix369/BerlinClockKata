6c280ab367cea2a50a310f7f4fbd50bf93aa5327
Mani Sarkar <mani@codurance.com>
2016-07-20 23:07:39 +0100

    Replaced logic with array by logic with a collection (list)

--- src/main/java/berlinclock/ClockRows.java
+++ src/main/java/berlinclock/ClockRows.java
@@ -3,20 +3,25 @@ package berlinclock;
 import static java.lang.String.format;
 import static berlinclock.LampState.LAMP_OFF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class ClockRows {
   protected static final int FIVE_HOURS = 5;
   protected static final int FIVE_MINUTES = 5;
 
-  protected String populateARowWithLamps(int count, LampState lampState, String rowFormatter) {
-    LampState[] row = new LampState[] {LAMP_OFF, LAMP_OFF, LAMP_OFF, LAMP_OFF};
+  private static final int MAX_LAMPS_PER_ROW = 4;
 
-    for (int index=0; index<row.length; index++) {
-      if (index < count) {
-        row[index] = lampState;
-      }
+  protected String populateARowWithLamps(int count, LampState lampState, String rowFormatter) {
+    List<LampState> lamps = new ArrayList<>();
+
+    int index = 0;
+    while (index < MAX_LAMPS_PER_ROW) {
+      lamps.add(index < count ? lampState : LAMP_OFF);
+      index++;
     }
 
-    return format(rowFormatter, row);
+    return format(rowFormatter, lamps.toArray());
   }
 
   public abstract String get();
